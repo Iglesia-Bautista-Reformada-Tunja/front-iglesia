c6f85a2470faab9370ac058f9d9ec9ae238a0cc2
Jhon Fa <jhonfaverm@gmail.com>
2023-12-30 02:25:22 -0500

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/pagina_iglesia')));
+app.use(express.static('./dist/pagina_iglesia'));
 
 app.get("/", (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "dist/pagina_iglesia") });
+    res.sendFile("index.html",{ root: "dist/pagina_iglesia" })
 });
 
 
